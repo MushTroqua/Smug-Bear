dafdbc8e1341cc52540cae3b159f2a353764328c
MushTroqua <70004111+MushTroqua@users.noreply.github.com>
2021-02-04 20:59:47 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,35 +4,36 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 client.on('ready', async () => {
+        client.user.setActivity("with these hands")
         console.log('Ready to fuck those motherfuckers up.');
-    
+
 });
-    
+
     const prefix = "s!";
         client.on("message", function(message) {
           if (message.author.bot) return;
           if (!message.content.startsWith(prefix)) return;
-            
+
         const commandBody = message.content.slice(prefix.length);
         const args = commandBody.split(' ');
         const command = args.shift().toLowerCase();
-            
+
         //This is the ping and pong command. Pretty standard.
             if (command === "ping") {
             const timeTaken = Date.now() - message.createdTimestamp;
-                message.channel.send(`Pong, motherfucker. This message had a latency of ${timeTaken}ms.`); 
+                message.channel.send(`Pong, motherfucker. This message had a latency of ${timeTaken}ms.`);
             }
-            
+
         //This is the display avatar command.
             const embed = new Discord.MessageEmbed()
                 .setColor('#4985e9')
                 .setTitle('🖼Avatar🖼')
                 .setImage(message.author.avatarURL())
-            
+
              if (command === 'avatar') {
             message.channel.send(embed);
-            }     
-            
+            }
+
         });
 
 client.login(config.BOT_TOKEN);
